4477cb82fb666dcf85f6b1685bb74c4e8470621f
Cheng-F <214phoebe@gmail.com>
2018-11-29 14:50:01 -0500

    Update pipelines.py

--- my_scraper/my_scraper/pipelines.py
+++ my_scraper/my_scraper/pipelines.py
@@ -12,18 +12,13 @@ from models import Deals, db_connect, create_deals_table
 class LivingSocialPipeline(object):
     """Livingsocial pipeline for storing scraped items in the database"""
     def __init__(self):
-        """
-        Initializes database connection and sessionmaker.
-        Creates deals table.
-        """
+       
         engine = db_connect()
         create_deals_table(engine)
         self.Session = sessionmaker(bind=engine)
 
     def process_item(self, item, spider):
         """Save deals in the database.
-
-        This method is called for every item pipeline component.
 
         """
         session = self.Session()
