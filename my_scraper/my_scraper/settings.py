188629476d76aaba1a1377ea9536c061fe59cd07
Cheng-F <214phoebe@gmail.com>
2018-11-29 14:50:36 -0500

    Update settings.py

--- my_scraper/my_scraper/settings.py
+++ my_scraper/my_scraper/settings.py
@@ -1,18 +1,9 @@
-# Scrapy settings for my_scraper project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
 BOT_NAME = 'livingsocialscraper'
 
 SPIDER_MODULES = ['my_scraper.spiders']
 NEWSPIDER_MODULE = 'my_scraper.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'my_scraper (+http://www.yourdomain.com)'
+ 
 
 DATABASE = {'drivername': 'postgres',
             'host': 'localhost',
